ab0fc3c5697e580192ed2432ff0d102fa6afdc1f
bca <bca@Bcalab4-m20>
2023-02-20 09:33:55 +0530

    first commit..!!

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Project1","c":"Car","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ src/Project1/Car.java
@@ -0,0 +1,72 @@
+package Project1;
+/**
+ * 
+ * @author RBCA-21-132
+ *  @version(1)
+ *  
+ * <p><b>This class stimulates the working of Car</b></p>
+ * <p>It does the following </p>
+ * It gets the speed and computes it,<br>
+ * when it is incremented,decremented and when the car is stopped
+ *
+ */
+public class Car {
+   String model;
+   int speed;
+   /**
+    * <p> Constructorr Car is been used to initialise the two attributes that is<br>
+    * model and speed
+    * @param model- used to get the name of car model
+    * @param speed-used to get the speed of the car
+    */
+    Car (String model,int speed)
+    {
+    	this.model=model;
+    	this.speed=speed;
+    }
+  
+    	/**
+    	 * <p>The method go_faster() is used to calculate the speed by incrementing it</p>
+    	 * @param increment-value is used to add to  the speed
+    	 */
+    void go_faster(int increment)
+    {
+    	speed=speed+increment;
+    }
+    /**
+     * <p>The method go_slower() is used to calculate the speed by incrementing it</p>
+     * @param decrement-value is used to minus to  the speed
+     */
+    void go_slower(int decrement)
+    {
+    	speed=speed- decrement;
+    }
+    /**
+     * <p>In stop_car the speed is set to zero <br>when the car is stopped</p>
+     */
+    void stop_car()
+    {
+    	speed=0;
+    }
+    /**
+     * <p>This function get_speed()</p>
+     * @return -returns the speed of the car
+     */
+    int get_speed()
+    {
+    	return speed;
+    }
+    /**
+     * <p>This the the main function from where,execution of the program takes place</p>
+     * @param args -take the arguments
+     */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Car C1=new Car("HONDA",60);
+       C1.go_faster(10);
+       C1.go_slower(10);
+       C1.stop_car();
+       	C1.get_speed();
+	}
+
+}
